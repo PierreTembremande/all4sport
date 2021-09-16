137312e6ae4e353f486859398b2c1a2b642f1be7
PierreTembremande <pierre.tembremande@gmail.com>
2021-09-16 16:55:30 +0200

    première ébauche de l'application web

---
+++ produits.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <link rel = "styleSheet" href = "style.css">
+    <title>Produits</title>
+  </head>
+  <body>
+
+    <h1> Produits </h1>
+       <div>
+
+           <button><a href="accueil.php">Accueil</a></button>
+           <button><a href="entrepots.php">Nos entrepôts</a></button>
+           <button><a href="produits.php">Notre liste de produits</a></button>
+           <button><a href="tableauDeBord.php">Notre tableau de bord</a></button>
+       </div>
+       <br><br><br>
+       <div id="produitsPhares">
+         <div class=produits>
+           <p> Produit 1
+
+        </div>
+         <div class=produits>
+          <p> Produit 2
+
+         </div>
+         <div class=produits>
+           <p> Produit 3
+
+           </div>
+           <div class=produits>
+             <p> Produit 4
+
+          </div>
+           <div class=produits>
+            <p> Produit 5
+
+           </div>
+           <div class=produits>
+             <p> Produit 6
+
+             </div>
+         </div>
+     </body>
+     </html>
---
+++ tableauDeBord.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <link rel = "styleSheet" href = "style.css">
+    <title>Tableau phares</title>
+  </head>
+  <body>
+
+    <h1> Tableau de bord </h1>
+       <div>
+
+           <button><a href="accueil.php">Accueil</a></button>
+           <button><a href="entrepots.php">Nos entrepôts</a></button>
+           <button><a href="produits.php">Notre liste de produits</a></button>
+           <button><a href="tableauDeBord.php">Notre tableau de bord</a></button>
+       </div>
+     </body>
+     </html>
